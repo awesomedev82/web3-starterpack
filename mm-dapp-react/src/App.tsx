62cc98f626d636e6730b6a56c5c845f0a9732a52
awesomedev82 <awesomedev08@gmail.com>
2023-06-30 23:24:23 +0300

    few more changes

--- mm-dapp-react/src/App.tsx
+++ mm-dapp-react/src/App.tsx
@@ -4,22 +4,59 @@ import detectEthereumProvider from '@metamask/detect-provider'
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const initialState = { accounts: [] }
+  const [wallet, setWallet] = useState(initialState)
 
   useEffect(() => {
+    const refreshAccounts = (accounts: any) => {                /* New */
+      if (accounts.length > 0) {                                /* New */
+        updateWallet(accounts)                                  /* New */
+      } else {                                                  /* New */
+        // if length 0, user is disconnected                    /* New */
+        setWallet(initialState)                                 /* New */
+      }                                                         /* New */
+    }                                                           /* New */
+
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true })
-      console.log(provider)
-      setHasProvider(Boolean(provider)) // transform provider to true or false
+      setHasProvider(Boolean(provider))
+
+      if (provider) {                                           /* New */
+        const accounts = await window.ethereum.request(         /* New */
+          { method: 'eth_accounts' }                            /* New */
+        )                                                       /* New */
+        refreshAccounts(accounts)                               /* New */
+        window.ethereum.on('accountsChanged', refreshAccounts)  /* New */
+      }                                                         /* New */
     }
 
     getProvider()
+    return () => {                                              /* New */
+      window.ethereum?.removeListener('accountsChanged', refreshAccounts)
+    }                                                           /* New */
   }, [])
+
+  const updateWallet = async (accounts:any) => {
+    setWallet({ accounts })
+  }
+
+  const handleConnect = async () => {
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    })
+    updateWallet(accounts)
+  }
 
   return (
     <div className="App">
       <div>Injected Provider {hasProvider ? 'DOES' : 'DOES NOT'} Exist</div>
-      { hasProvider &&
-        <button>Connect MetaMask</button>
+
+      { window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&  /* Updated */
+        <button onClick={handleConnect}>Connect MetaMask</button>
+      }
+
+      { wallet.accounts.length > 0 &&
+        <div>Wallet Accounts: { wallet.accounts[0] }</div>
       }
     </div>
   )
